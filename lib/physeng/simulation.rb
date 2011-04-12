75b54949c50ce0bdde21dbebe0a0aa211ae9a63f
Ilkka Laukkanen <ilkka.laukkanen@sasken.com>
2011-04-12 12:52:39 +0300

    Paint 10 random pixels onscreen

--- lib/physeng/simulation.rb
+++ lib/physeng/simulation.rb
@@ -4,15 +4,26 @@ class Physeng
   class Simulation
     require 'physeng/simulation/particle'
 
+    SCREEN_WIDTH = 400
+    SCREEN_HEIGHT = 400
+
     def initialize
       SDL::init(SDL::INIT_EVERYTHING)
       @particles = (1..10).inject([]) do |particles,num|
-        particles << Physeng::Simulation::Particle.new([rand, rand], [rand, rand])
+        particles << Physeng::Simulation::Particle.new(rand, rand, rand, rand)
       end
     end
 
     def run
-      @screen = SDL::set_video_mode(400, 400, 8, SDL::SWSURFACE)
+      @screen = SDL::set_video_mode(SCREEN_WIDTH, SCREEN_HEIGHT, 8, SDL::SWSURFACE)
+      dirty = @particles.inject([]) do |rects,p|
+        screenx = p.x * SCREEN_WIDTH
+        screeny = p.y * SCREEN_HEIGHT
+        @screen.put_pixel(screenx, screeny, @screen.map_rgb(0, 255, 0))
+        rects << [screenx, screeny, 1, 1]
+      end
+      @screen.update_rects *dirty
+      sleep 2
       SDL::quit
       return 0
     end
