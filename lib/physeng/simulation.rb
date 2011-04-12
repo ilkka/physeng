fb02e153993a23a451472e3b2e62db50f0c41c54
Ilkka Laukkanen <ilkka.laukkanen@sasken.com>
2011-04-12 15:53:35 +0300

    Adjust particle speeds for constant animation rate

--- lib/physeng/simulation.rb
+++ lib/physeng/simulation.rb
@@ -55,7 +55,7 @@ class Physeng
 
     def random_particle
       Particle.new(@rng.rand(-1.0..1.0), @rng.rand(-1.0..1.0),         # x, y
-                   @rng.rand(-0.03..0.03), @rng.rand(-0.03..0.03),         # xvel, yvel
+                   @rng.rand(-0.8..0.8), @rng.rand(-0.8..0.8),         # xvel, yvel
                    3.times.inject([]) {|l,i| l << @rng.rand(0..255)})  # [r, g, b]
     end
 
