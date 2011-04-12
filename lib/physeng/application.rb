4832cb7b516b9df7b290ef4f9bafb5c5ad8ac9ca
Ilkka Laukkanen <ilkka.laukkanen@sasken.com>
2011-04-12 12:14:13 +0300

    Basic option handling structure

--- lib/physeng/application.rb
+++ lib/physeng/application.rb
@@ -1,8 +1,16 @@
+require 'trollop'
+
 class Physeng
   class Application
     class << self
       def run!(*arguments)
-
+        p = Trollop::Parser.new do
+        end
+        @opts = Trollop::with_standard_exception_handling p do
+          o = p.parse arguments
+          #raise Trollop::HelpNeeded if arguments.empty?
+          o
+        end
       end
     end
   end
