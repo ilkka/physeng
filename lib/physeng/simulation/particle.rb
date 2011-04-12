6cb32f73063c15ec6e349cbb1fd025cc8b47edd4
Ilkka Laukkanen <ilkka.laukkanen@sasken.com>
2011-04-12 12:48:22 +0300

    Fix initialization

--- lib/physeng/simulation/particle.rb
+++ lib/physeng/simulation/particle.rb
@@ -3,7 +3,10 @@ class Physeng
     class Particle
       attr_reader :x, :y, :xvel, :yvel
       def initialize(x, y, xvel, yvel)
-        (@x, @y, @xvel, @yvel) = (x, y, xvel, yvel)
+        @x = x
+        @y = y
+        @xvel = xvel
+        @yvel = yvel
       end
     end
   end
