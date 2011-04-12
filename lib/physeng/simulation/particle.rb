e67cf84035b7da251965695bb58fbc34e09716f4
Ilkka Laukkanen <ilkka.laukkanen@sasken.com>
2011-04-12 13:11:04 +0300

    Do separate velocity checks so particles can't bounce offscreen
    I guess it might be possible for a particle to be offscreen for two updates, so
    that the old type of check could just bounce it there, always reversing it's
    velocity? The separate check makes this explicit

--- lib/physeng/simulation/particle.rb
+++ lib/physeng/simulation/particle.rb
@@ -14,21 +14,29 @@ class Physeng
         @x += @xvel
         @y += @yvel
         # collide from screen edges
-        if @x <= 0 && @xvel < 0
+        if @x <= 0
           @x = 0
-          @xvel = -@xvel
+          if @xvel < 0
+            @xvel = -@xvel
+          end
         end
-        if @x >= 1 && @xvel > 0
+        if @x >= 1
           @x = 1
-          @xvel = -@xvel
+          if @xvel > 0
+            @xvel = -@xvel
+          end
         end
-        if @y <= 0 && @yvel < 0
+        if @y <= 0
           @y = 0
-          @yvel = -@yvel
+          if @yvel < 0
+            @yvel = -@yvel
+          end
         end
-        if @y >= 1 && @yvel > 0
+        if @y >= 1
           @y = 1
-          @yvel = -@yvel
+          if @yvel > 0
+            @yvel = -@yvel
+          end
         end
       end
     end
