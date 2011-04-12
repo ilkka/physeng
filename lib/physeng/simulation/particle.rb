f083bcde5d498aada97976f2f988cc55fe58d179
Ilkka Laukkanen <ilkka.laukkanen@sasken.com>
2011-04-12 13:05:43 +0300

    Collide particles from screen edges

--- lib/physeng/simulation/particle.rb
+++ lib/physeng/simulation/particle.rb
@@ -13,6 +13,23 @@ class Physeng
       def move!
         @x += @xvel
         @y += @yvel
+        # collide from screen edges
+        if @x <= 0 && @xvel < 0
+          @x = 0
+          @xvel = -@xvel
+        end
+        if @x >= 1 && @xvel > 0
+          @x = 1
+          @xvel = -@xvel
+        end
+        if @y <= 0 && @yvel < 0
+          @y = 0
+          @yvel = -@yvel
+        end
+        if @y >= 1 && @yvel > 0
+          @y = 1
+          @yvel = -@yvel
+        end
       end
     end
   end
