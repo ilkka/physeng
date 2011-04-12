459090a59afd6a240df87b0228b41b39661040b1
Ilkka Laukkanen <ilkka.laukkanen@sasken.com>
2011-04-12 12:59:37 +0300

    define move! method for Particle that moves it

--- lib/physeng/simulation/particle.rb
+++ lib/physeng/simulation/particle.rb
@@ -2,11 +2,17 @@ class Physeng
   class Simulation
     class Particle
       attr_reader :x, :y, :xvel, :yvel
+
       def initialize(x, y, xvel, yvel)
         @x = x
         @y = y
         @xvel = xvel
         @yvel = yvel
+      end
+
+      def move!
+        @x += @xvel
+        @y += @yvel
       end
     end
   end
