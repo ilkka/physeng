6760e580fbc5fc50ac08a6f3ffe316d7e2ac9973
Ilkka Laukkanen <ilkka.laukkanen@sasken.com>
2011-04-12 12:58:16 +0300

    Run for ten seconds in a 30 ms game loop

--- lib/physeng/simulation.rb
+++ lib/physeng/simulation.rb
@@ -6,6 +6,7 @@ class Physeng
 
     SCREEN_WIDTH = 400
     SCREEN_HEIGHT = 400
+    UPDATE_INTERVAL = 30
 
     def initialize
       SDL::init(SDL::INIT_EVERYTHING)
@@ -16,16 +17,31 @@ class Physeng
 
     def run
       @screen = SDL::set_video_mode(SCREEN_WIDTH, SCREEN_HEIGHT, 8, SDL::SWSURFACE)
-      dirty = @particles.inject([]) do |rects,p|
-        screenx = p.x * SCREEN_WIDTH
-        screeny = p.y * SCREEN_HEIGHT
-        @screen.put_pixel(screenx, screeny, @screen.map_rgb(0, 255, 0))
-        rects << [screenx, screeny, 1, 1]
+      @next_update = SDL::get_ticks + UPDATE_INTERVAL
+      while @next_update < 10000
+        dirty = @particles.inject([]) do |rects,p|
+          screenx = p.x * SCREEN_WIDTH
+          screeny = p.y * SCREEN_HEIGHT
+          @screen.put_pixel(screenx, screeny, @screen.map_rgb(0, 255, 0))
+          rects << [screenx, screeny, 1, 1]
+        end
+        @screen.update_rects *dirty
+        SDL::delay time_to_next_update
+        @next_update += UPDATE_INTERVAL
       end
-      @screen.update_rects *dirty
-      sleep 2
       SDL::quit
       return 0
+    end
+
+    private
+
+    def time_to_next_update
+      now = SDL::get_ticks
+      if @next_update <= now
+        0
+      else
+        @next_update - now
+      end
     end
   end
 end
