b47c6c9173a501cc3517c50bfb51b044df216f5d
Ilkka Laukkanen <ilkka.s.laukkanen@gmail.com>
2011-04-14 00:24:58 +0300

    Descriptive comments

--- lib/physeng/simulation.rb
+++ lib/physeng/simulation.rb
@@ -64,9 +64,9 @@ class Physeng
       Particle.new(@rng.rand(-1.0..1.0), @rng.rand(-1.0..1.0),         # x, y
                    @rng.rand(-0.8..0.8), @rng.rand(-0.8..0.8),         # xvel, yvel
                    3.times.inject([]) {|l,i| l << @rng.rand(0..255)},  # [r, g, b]
-                   @rng.rand(0.3..1.0),
-                   @rng.rand(1.0..100.0),
-                   @rng.rand(0.01..0.10))
+                   @rng.rand(0.3..1.0),                                # coefficient of restitution
+                   @rng.rand(1.0..100.0),                              # mass
+                   @rng.rand(0.01..0.10))                              # radius
     end
 
     def clear_screen
